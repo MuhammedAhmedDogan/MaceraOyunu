c0a11f4d96023ebfbfe053ccb2ff1db81a380d0c
Ahmed Dogan <114875634+MuhammedAhmedDogan@users.noreply.github.com>
2024-03-14 05:30:32 +0300

    v7

--- src/locations/Mine.java
+++ src/locations/Mine.java
@@ -3,8 +3,16 @@ package locations;
 import enemies.*;
 import player.*;
 
-public class Mine extends BattleLocation{
+import java.util.Random;
+
+public class Mine extends BattleLocation {
     public Mine(Player player) {
         super(6, player, "Maden", "    :\tYılan yuvası", new Snake(), "Ödül: Her öldürülen yılandan Silah, Zırh veya Para düşebilir", 5);
+        setDamage();
+    }
+
+    public void setDamage() {
+        Random random = new Random();
+        this.getObstacle().setDamage (random.nextInt(4) + 3);
     }
 }
